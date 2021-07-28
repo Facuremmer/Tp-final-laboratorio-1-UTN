953616ac6e158405653252f2ae228f79d2c7466d
FACUNDOREMMD975\facuremmer <facundoremmer0@gmail.com>
2021-07-27 21:08:19 -0300

    Api + algunos arreglos en lo visual (hice otro archivo js porque si estaba todo en el mismo no andaba)

--- IndexFacu.js
+++ IndexFacu.js
@@ -1,27 +1,4 @@
-const navToggle = document.querySelector(".nav-toggle");
-const navMenu = document.querySelector(".nav-menu");
-
-navToggle.addEventListener("click", () => {
-  navMenu.classList.toggle("nav-menu_visible");
-
-  if (navMenu.classList.contains("nav-menu_visible")) {
-    navToggle.setAttribute("aria-label", "Cerrar menú");
-  } else {
-    navToggle.setAttribute("aria-label", "Abrir menú");
-  }
-});
-
-const menuLinks = document.querySelectorAll('.nav-menu a[href^="#"]');
-  menuLinks.forEach(menuLinks => {
-    menuLinks.addEventListener("click", function(){
-      navMenu.classList.remove("nav-menu_visible");
-    })
-
-    // const SUBnavToggle = document.querySelectorAll(".SUBmenubtn");
-
-  });
-
-// API
+  // API
 const cargarImagenes=async()=>{
   // RECOGEMOS LO QUE INGRESO EL USUARIO
   let input=document.querySelector("#busqueda").value;
@@ -81,14 +58,9 @@ setTimeout(()=>{
 }
 ,3000)
 
-
-
-
   });
-
-
-
 }
+
 
 
 
